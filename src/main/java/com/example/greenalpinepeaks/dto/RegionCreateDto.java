d6966b0231e2ab7ef269531f77401a7bad30c6ec
ELA <0347030518@my.lerna.by>
2026-04-03 13:32:07 +0300

    JPA (Hibernate/Spring Data)

--- src/main/java/com/example/greenalpinepeaks/dto/RegionCreateDto.java
+++ src/main/java/com/example/greenalpinepeaks/dto/RegionCreateDto.java
@@ -1,17 +1,13 @@
 package com.example.greenalpinepeaks.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class RegionCreateDto {
 
     @NotBlank
     private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
